8fe4cc2bc6fe18c813b8d3730fb77aa40ecd1eaa
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-11 10:31:19 -0400

    Update Entity.java

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,3 +1,16 @@
 
 public abstract class Entity
 {
+  private double speed, hitAccuracy, 
+  private int health, hitDamage, 
+  
+  public double getHitAccuracy()
+  {
+    return hitAccuracy;
+  }
+  
+  public double hitDamage()
+  {
+    return hitDamage;
+  }
+}
