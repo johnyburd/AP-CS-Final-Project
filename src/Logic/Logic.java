b9867e09b2362114fe0d4e3dde6f5874e9cfcbed
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-11 10:14:43 -0400

    Update Logic.java

--- src/Logic/Logic.java
+++ src/Logic/Logic.java
@@ -1,7 +1,20 @@
 
 public class Logic 
 {
-  public void onExitMap()
+  public void onExitLevel()
+  {
+    //win or go to next level;
+  }
+  
+  public boolean isDead(Player e)
+  {
+    if(e.getHealth() == 0)
+    {
+      //load the death animmation and screen overlay
+    }
+  }
+  
+  public void onFight()
   {
     
   }
