c3a84a368dec35ad126222856cfd24443c579ff7
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-12 10:06:41 -0400

    Update Shield.java

--- src/logic/armor/Shield.java
+++ src/logic/armor/Shield.java
@@ -1,6 +1,28 @@
 package src.logic.armor;
 
+import src.engine.Keyboard;
+
 public class Shield extends Armor
 {
-
+  private boolean isRaised;
+  
+  public Shield(int dur, int AC, String str)
+  {
+    super(dur, AC, str);
+  }
+  
+  public boolean isUp()
+  {
+    return isRaised;
+  }
+  
+  public void setShieldUp()
+  {
+    isRaised = true;
+  }
+  
+  public void setShieldDown()
+  {
+    isRaised = false;
+  }
 }
