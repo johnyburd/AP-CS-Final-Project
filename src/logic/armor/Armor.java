17212cd9c17c5a7524cd2cdb5ba79cfc2abfa3d0
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-11 21:58:58 -0400

    Update Armor.java

--- src/logic/armor/Armor.java
+++ src/logic/armor/Armor.java
@@ -1,1 +1,24 @@
+package src.logic.armor;
 
+public class Armor
+{
+  private int durability, armorClass;
+  private String name;
+  
+  public int getDurability()
+  {
+    return durability;
+  }
+  
+  public int getArmorClass()
+  {
+    return armorClass;
+  }
+  
+  public Armor(int dur, int aC, String str)
+  {
+    durability = dur;
+    armorClass = aC;
+    name = str;
+  }
+}
