3ccd24301b0bc64855b583a8ee880f22641337de
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-12 09:53:22 -0400

    Update Player.java

--- src/logic/player/Player.java
+++ src/logic/player/Player.java
@@ -1,1 +1,22 @@
+package src.logic.player;
 
+public class Player
+{
+  private int health;
+  private final double reach = 1.5;
+  
+  public int getHealth()
+  {
+    return health;
+  }
+  
+  public int getReach()
+  {
+    return reach;
+  }
+  
+  public final setHealth(int h)
+  {
+    health = h;
+  }
+}
