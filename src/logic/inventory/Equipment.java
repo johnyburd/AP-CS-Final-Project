b1e1395170a447e1b9bf8269655f4d2b87dea935
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-13 13:47:18 -0400

    made two new constructors, for use in monster generation

--- src/logic/inventory/Equipment.java
+++ src/logic/inventory/Equipment.java
@@ -1,3 +1,5 @@
+package src.logic.inventory;
+
 import src.logic.weapon.Weapon;
 import src.logic.armor.Armor;
 import src.logic.armor.Shield;
@@ -7,6 +9,22 @@ public class Equipment
 	public Weapon primaryWeapon = weaponArray[0];
 	public Armor theArmor;
 	public Shield theShield;
+	
+	// This default constructor is for monsters only, allows basic opponents with no equipment
+	public Equipment()
+	{
+		primaryWeapon = null;
+		theArmor = null;
+		theShield = null;
+	}
+	
+	// This constructor is for monsters only, as they cannot weild shields ever
+	public Equipment(Weapon weap, Armor arm)
+	{
+		primaryWeapon = weap;
+		theArmor = arm;
+		theShield = null;
+	}
 	
 	public Equipment(Weapon weap, Armor arm, Shield shi)
 	{
