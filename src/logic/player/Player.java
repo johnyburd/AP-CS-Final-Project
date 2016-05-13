40ae14d181bda5d0f4534b43813f1c25ad38c3df
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-12 23:06:04 -0400

    Update Player.java

--- src/logic/player/Player.java
+++ src/logic/player/Player.java
@@ -1,9 +1,39 @@
 package src.logic.player;
+import src.logic.inventory.Equipment;
+import src.logic.weapon.Weapon;
+import src.logic.armor.*;
 
 public class Player
 {
   private int health;
   private final double reach = 1.5;
+  private Equipment equippedItems;
+ 
+ public Player(int hlt, Weapon weapon, Armor armor, Shield shield)
+ {
+   health = hlt;
+   equippedItems = new Equipment(weapon, armor, shield);
+ }
+ 
+ public boolean hasShield()
+ {
+   return equippedItems.getShield() != null;
+ }
+ 
+ public Shield getEquippedShield()
+ {
+   return equippedItems.getShield();
+ }
+ 
+ public Armor getEquippedArmor()
+ {
+   return equippedItems.getArmor();
+ }
+ 
+ public Weapon getEquippedWeapon()
+ {
+   return equippedItems.getWeapon();
+ }
   
   public int getHealth()
   {
@@ -18,5 +48,15 @@ public class Player
   public final setHealth(int h)
   {
     health = h;
+  }
+  
+  public int getTotalArmorClass()
+  {
+    int ac = equippedItems.getArmor();
+    if(equippedItems.getShield().isRaised() == true)
+    {
+      ac += equippedItems.getShield().getArmorClass();
+    }
+    return ac;
   }
 }
