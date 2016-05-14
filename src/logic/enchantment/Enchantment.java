18f9f58f70c8c43e017303f71c06b9bbdaff4ab2
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-13 22:46:26 -0400

    Update Enchantment.java

--- src/logic/enchantment/Enchantment.java
+++ src/logic/enchantment/Enchantment.java
@@ -2,5 +2,45 @@ package src.logic.Enchantment;
 
 public class Enchantment
 {
+  public static boolean isEnchanted(Weapon weap)
+  {
+    return weap.isEnchanted();
+  }
   
+  public static boolean isEnchanted(Armor arm)
+  {
+    return arm.isEnchanted();
+  }
+  
+  public static boolean isEnchanted(Shield shi)
+  {
+    return shi.isEnchanted();
+  }
+  
+  public static void enchantWeapon(Weapon weap)
+  {
+    int i = (int)(Math.random() * 2);
+    int amt = (int)(Math.ranom() * 4) + 1;
+    if(i == 0)
+      weap.setDurability(weap.getDurability() + amt);
+    else if(i == 1)
+      weap.setWeaponDamage(weap.getWeaponDamage() + amt)
+    weap.setName(weap.getName() + " + " + amt);
+  }
+  
+  public static void enchantArmor(Armor arm)
+  {
+    int i = (int)(Math.random() * 2);
+    int amt = (int)(Math.random() * 4) + 1;
+    if(i == 0)
+      arm.setDurability(arm.getDurability + amt);
+    else if(i == 1)
+      arm.setArmorClass(arm.getArmorClass() + amt);
+    arm.setName(arm.getName() + " + " + amt);
+  }
+  
+  public static void enchantShield(Shield shi)
+  {
+    enchantArmor(shi);
+  }
 }
