d9e66c8b8787e032bc74ad886d52dfb6cee70a28
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-14 21:39:03 -0400

    Update Entity.java

--- src/logic/entity/Entity.java
+++ src/logic/entity/Entity.java
@@ -78,17 +78,6 @@ public abstract class Entity
     return prob >= this.getHitAccuracy();
   }
   
-  /*
-  public void onMonsterAttack()
-  {
-    if(monsterHitSuccessful())
-      onMonsterHit();
-    for(double i = 10.0; i > this.getSpeed(); i -= .5)
-    {
-    }
-  }
-  */
-  
   public void setEquippedWeapon(Weapon weap)
   {
     equippedItems.switchWeapon(weap);
@@ -97,34 +86,5 @@ public abstract class Entity
   public void setEquippedArmor(Armor arm)
   {
     equippedItems.switchArmor(arm);
-  }
-  
-  public void onMonsterHit(Player p)
-  {
-    int dmg = this.getHitDamage();
-    if(getEquippedWeapon() != null)
-      dmg += getEquippedWeapon().getWeaponDamage();
-    int playerDmg = dmg - p.getTotalArmorClass();
-    dmg -= playerDmg;
-    int armorDmg = dmg, shieldDmg = 0;
-    if(p.getEquippedShield() != null && p.getEquippedShield().isRaised())
-    {
-      armorDmg = dmg - p.getEquippedShield().getArmorClass();
-      shieldDmg = dmg - armorDmg;
-      p.getEquippedShield().setDurability(p.getEquippedShield.getDurability() - shieldDmg);
-    }
-    if(p.getEquippedArmor != null)
-    {
-      p.getEquippedArmor().setDurability(p.getEquippedArmor().getDurability() - armorDmg);
-      if(this.getEquippedWeapon() != false)
-        this.getEquippedWeapon().setDurability(getEquippedWeapon().getDurability() - p.getTotalArmorClass());
-    }
-    else if(this.getEquippedWeapon() != false)
-      if(this.getEquippedWeapon().getName() != "Fist")
-      {  
-        this.getEquippedWeapon().setDurability(getEquippedWeapon.getDurability() - 3);
-        if(doesWeaponBreak() == true)
-          this.setEquippedWeapon(null);
-      }
   }
 }
