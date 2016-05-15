b6b2d61c247171f60b7e4f27ed7ad22299fb4b34
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-15 12:07:31 -0400

    Created a Chest class, randomly gives a bonus

--- src/logic/inventory/Chest.java
+++ src/logic/inventory/Chest.java
@@ -1,1 +1,28 @@
+package src.logic.inventory;
 
+import src.logic.player.Player;
+import src.logic.weapon.Weapon;
+import src.logic.armor.*;
+
+public class Chest
+{
+  public void onOpenChest(Player p)
+  {
+    int i = (int)(Math.random() * 4);
+    if(i == 0)
+      p.setHealth(100);
+    else if(i == 1)
+    {
+      int a = (int)(Math.random * Weapon.weaponArray.length);
+      //p.changeEquippedWeapon(Weapon.weaponArray[a]);
+    }
+    else if(i == 2)
+    {
+      
+    }
+    else if(i == 3)
+    {
+      
+    }
+  }
+}
