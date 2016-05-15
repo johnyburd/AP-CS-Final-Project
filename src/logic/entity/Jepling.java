dfa04d48dd402cc17820ffaf1d27a6b71361dda8
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-14 22:07:12 -0400

    Update Jepling.java

--- src/logic/entity/Jepling.java
+++ src/logic/entity/Jepling.java
@@ -1,5 +1,9 @@
 package src.logic.entity;
 
+import src.logic.inventory.Equipment;
+import src.logic.weapon.*;
+import src.logic.armor.*;
+
 public class Jepling extends Entity
 {
   public Jepling()
@@ -7,7 +11,6 @@ public class Jepling extends Entity
     super(1, 88.88, 250, 25, new Equipment());
   }
   
-  @override
   public int getTotalArmorClass()
   {
     return 10;
