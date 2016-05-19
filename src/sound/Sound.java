372651b4b5569d1b46437a8e2542068c86e6e05d
Jaynv24 <jaynv16@gmail.com>
2016-05-18 20:31:33 -0400

    Update Sound.java

--- src/sound/Sound.java
+++ src/sound/Sound.java
@@ -1,25 +1,140 @@
-import java.applet.Applet;
-import java.applet.AudioClip;
-public class Sound 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import javax.sound.sampled.*;
+public class Sound
 {
-    public static synchronized void play(final String fileName)
-        new Thread(new Runnable() 
-        {
-            public void run() 
-            {
-                try 
-                {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(fileName));
-                    clip.open(inputStream);
-                    clip.start(); 
-                }
-                catch (Exception e) 
-                {
-                    System.out.println("play sound error: " + e.getMessage() + " for " + fileName);
-                }
-            }
-        }
-        ).start();
-    }
+	public static void playPlayerHit()
+	{
+		try 
+   	 	{
+    		File file = new File("playerhit.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+	
+	public static void playWalking()
+	{
+		try 
+   	 	{
+    		File file2 = new File("footsteps.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file2));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+	
+	public static void playEnemyGrunt()
+	{
+		try 
+   	 	{
+    		File file3 = new File("enemygrunt.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file3));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+	
+	public static void playEnemyHit()
+	{
+		try 
+   	 	{
+    		File file4 = new File("enemyhit.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file4));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+	
+	public static void playGameOver()
+	{
+		try 
+   	 	{
+    		File file5 = new File("gameover.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file5));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+	
+	public static void playGameWon()
+	{
+		try 
+   	 	{
+    		File file6 = new File("gamewon.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file6));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+	
+	public static void playEnterSmileZone()
+	{
+		try 
+   	 	{
+    		File file7 = new File("smilezoneenter.wav");
+         
+     	   	Clip clip = AudioSystem.getClip();
+        	clip.open(AudioSystem.getAudioInputStream(file7));
+        	clip.start();
+        
+        	Thread.sleep(clip.getMicrosecondLength()/1000);
+   		} 
+    	catch(Exception ex) 
+    	{
+        	System.out.println("Error with playing sound.");
+        	ex.printStackTrace();
+   		}
+	}
+
 }
