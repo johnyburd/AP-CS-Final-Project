0bf06a6b1f35961c3178f10a3dce446e00474c73
ShauntSerelu <williamsschoolemail@gmail.com>
2016-05-19 06:41:11 -0400

    Update Weapon.java

--- src/logic/weapon/Weapon.java
+++ src/logic/weapon/Weapon.java
@@ -71,7 +71,7 @@ public class Weapon
     return this.getDurability() <= 0 && name != "Fist";
   }
   
-  public static Weapon[] weaponArray = {new Weapon(4, -1, 0, "Fist"), new Weapon(7, 15, 2, "Dull Sword"), new Weapon(10, 20, 2, "Decent Sword"), new Weapon(13, 25, 2, "Good Sword"), new Weapon(16, 30, 2, "Great Sword"), new Weapon(19, 25, 2.5, "Longsword"), new Weapon(25, 100, 2.1, "Master Sword")};
+  public static Weapon[] weaponArray = {new Weapon(4, -1, 0, "Fist"), new Weapon(7, 30, 2, "Dull Sword"), new Weapon(10, 40, 2, "Decent Sword"), new Weapon(13, 50, 2, "Good Sword"), new Weapon(16, 60, 2, "Great Sword"), new Weapon(19, 50, 2.5, "Longsword"), new Weapon(25, 100, 2.1, "Master Sword")};
   
   public String toString()
   {
