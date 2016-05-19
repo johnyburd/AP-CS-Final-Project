0ded23eb0f6d28e1dbe02842094a0692cda85fb9
Jaynv24 <jaynv16@gmail.com>
2016-05-19 02:29:43 -0400

    Update DungeonGenerator.java

--- src/dungeon/DungeonGenerator.java
+++ src/dungeon/DungeonGenerator.java
@@ -192,7 +192,7 @@ public class DungeonGenerator
                 if((a < 500 && numChests > 0 && dun[i][j] == 0 ) && ((dun[i][j-1] > 0 && dun[i+1][j] > 0 && dun[i][j-1] > 0) || (dun[i-1][j] > 0 && dun[i][j+1] > 0 && dun[i+1][j] > 0)))
                 {
                     numChests--;
-                   // dun[i][j] = 15;
+                    dun[i][j] = 15;
                     Chest chest = new Chest(((double)i + 0.5), j + 0.5, player, textbox);
                     chest.randomizeChest();
                     if (chest == null)
