e27d04a1dce7b29d2ddf7bd9708136342bf81aec
Jaynv24 <jaynv16@gmail.com>
2016-05-19 01:18:46 -0400

    Update Jepling.java

--- src/logic/entity/Jepling.java
+++ src/logic/entity/Jepling.java
@@ -5,13 +5,16 @@ import src.hud.TextBox;
 import src.logic.inventory.Equipment;
 import src.logic.weapon.*;
 import src.logic.armor.*;
+import src.engine.Player;
+import src.engine.Sprite;
 
 public class Jepling extends Entity
 {
   private TextBox textbo;
+  private Player play
   public Jepling()
   {
-    super(1.0, 88.88, 250, 25, new Equipment(), 25, 25, null);
+    super(1.0, 88.88, 250, 25, new Equipment(), 25, 25, null, play);
   }
   
   public int getTotalDamage()
